24e335eec1157fd6a44e4a66d822d25371fc2741
Lucel Da Silva <luceldasilva@yahoo.com>
2024-04-22 20:59:32 -0400

    ➖ [DELETE] borrar columna present_in_road_wcs_23
    No llegué a usar la columna porque tengo certeza de jugadores

---
+++ 02_update_database.sql
@@ -0,0 +1,10 @@
+/*
+	Modificaciones del dataset sobre detalles que quería cambiar
+	a lo largo del tiempo
+*/
+
+/*
+	No llegué a usar la columna porque tengo certeza de jugadores
+	que ocultaron su antiguo nick con el #
+*/
+ALTER TABLE players DROP COLUMN present_in_road_wcs_23;
